4104d410e053c42a69009d8233dd3c0714cf6681
Isagba gerare <gerardisagba17@gmail.com>
2025-12-02 17:47:21 +0100

    obtained username

---
+++ analyse.py
@@ -0,0 +1,8 @@
+#welcome message
+print("\n welcome to my Okj text analysis tool.")
+#username message
+print("Please enter your username:") 
+#username input
+username = input()
+#personalized greeting
+print("hello " + username + ", let's get started!")
